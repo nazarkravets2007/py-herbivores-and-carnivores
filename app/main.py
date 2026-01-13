61d2da72835eb1613361d22c28488a28d84b6c9c
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-01-13 13:59:25 +0200

    Update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
-    def __init__(self, name: str) -> None:
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
         self.name = name
         self.health = 100
         self.hidden = False
@@ -25,7 +28,10 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, animal: Animal) -> None:
+    def bite(
+        self,
+        animal: Animal,
+    ) -> None:
         if not isinstance(animal, Herbivore):
             return
 
@@ -36,35 +42,3 @@ class Carnivore(Animal):
         if animal.health <= 0:
             animal.health = 0
             animal._die()
-
-
-# ===== Example usage =====
-if __name__ == "__main__":
-    Animal.alive.clear()
-
-    lion = Carnivore("Simba")
-    assert len(Animal.alive) == 1
-    assert isinstance(Animal.alive[0], Carnivore)
-
-    rabbit = Herbivore("Susan")
-    rabbit.hide()
-    assert rabbit.hidden is True
-
-    rabbit.hide()
-    lion.bite(rabbit)
-    assert rabbit.health == 50
-
-    rabbit.hide()
-    lion.bite(rabbit)
-    assert rabbit.health == 50
-
-    rabbit.hide()
-    lion.bite(rabbit)
-    assert rabbit.health == 0
-    assert rabbit not in Animal.alive
-
-    Animal.alive.clear()
-    pantera = Carnivore("Bagira")
-    snake = Carnivore("Kaa")
-    print(Animal.alive)
-    # [{Name: Bagira, Health: 100, Hidden: False}, {Name: Kaa, Health: 100, Hidden: False}]
